986f284a0faed138f10eec3f1dea13df456aab78
marcsanuy <marcsanuy@gmail.com>
2020-07-21 11:41:52 +0200

    Use navigations for guests and users

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      
-    </div>
+    <b-container>
+        <navigation-guest v-if="!isLogged"></navigation-guest>
+        <navigation-logged v-else></navigation-logged>
     <router-view/>
+    </b-container>
   </div>
 </template>
+
+<script>
+  import NavigationLogged from '@/components/Navigations/Logged'
+  import NavigationGuest from '@/components/Navigations/Guest'
+  import {mapState} from 'vuex'
+  export default {
+    components: {
+      NavigationLogged, NavigationGuest
+    },
+    computed: {
+      ...mapState('auth', ['isLogged'])
+    }
+  }
+</script>
--- src/components/Navigations/Logged.vue
+++ src/components/Navigations/Logged.vue
@@ -1,7 +1,7 @@
 <template>
     <b-nav tabs>
         <b-nav-item exact to="/secret">Página secreta</b-nav-item>
-        <b-nav-item exact to="/secret/notes">Página secreta</b-nav-item>
+        <b-nav-item exact to="/secret/notes">Notas secretas</b-nav-item>
         <b-nav-item @click="_logout">Cerrar sesión</b-nav-item>
     </b-nav>
 </template>
