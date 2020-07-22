72998cccd873093fe0098d36c03a6c3464f8d142
marcsanuy <marcsanuy@gmail.com>
2020-07-22 11:10:43 +0200

    Config preloader on App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <loading :active.sync="loading"></loading>
     <b-container>
         <navigation-guest v-if="!isLogged"></navigation-guest>
         <navigation-logged v-else></navigation-logged>
@@ -12,12 +13,15 @@
   import NavigationLogged from '@/components/Navigations/Logged'
   import NavigationGuest from '@/components/Navigations/Guest'
   import {mapState} from 'vuex'
+  import Loading from 'vue-loading-overlay'
+  import 'vue-loading-overlay/dist/vue-loading.css'
   export default {
     components: {
-      NavigationLogged, NavigationGuest
+      NavigationLogged, NavigationGuest, Loading
     },
     computed: {
-      ...mapState('auth', ['isLogged'])
+      ...mapState('auth', ['isLogged']),
+      ...mapState(['loading'])
     }
   }
 </script>
