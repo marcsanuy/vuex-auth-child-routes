c3a5923747ccac072845fd7ebd17934046c0054a
marcsanuy <marcsanuy@gmail.com>
2020-07-22 11:27:33 +0200

    Start component login form

---
+++ src/components/Authentication/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        props: {
+            user:{
+                type: Object,
+                required: true,
+                validator: user => {
+                   if ( ! user.hasOwnProperty('email') || ! user.hasOwnProperty('password')) {
+                       console.warn('Usuario no válido')
+                       return false
+                   }
+                   return true
+                }
+            }
+        }
+    }
+</script>
