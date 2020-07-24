926349201e4696ec40021e28d02154d70517196d
marcsanuy <marcsanuy@gmail.com>
2020-07-24 11:02:01 +0200

    Finish login form component

--- src/components/Authentication/Login.vue
+++ src/components/Authentication/Login.vue
@@ -1,5 +1,48 @@
 <template>
-    
+    <b-form @submit.prevent="~emit('login')">
+        <b-form-group
+            label="Email"
+            description="Los datos son privados"
+        >
+            <b-form-input
+                type="email"
+                autocomplete="off"
+                v-model="user.email"
+                v-validate="'required|email'"
+                name="email"
+                placeholder="Introduce el email admin@vue.com"
+            ></b-form-input>
+            <b-form-invalid-feedback>
+                {{ errrors.first('email')}}
+            </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-form-group
+            label="Password"
+                    >
+            <b-form-input
+                type="password"
+                autocomplete="off"
+                v-model="user.password"
+                v-validate="'required|min:6'"
+                name="password"
+                placeholder="Introduce el password @Password1"
+            ></b-form-input>
+            <b-form-invalid-feedback>
+                {{ errrors.first('password')}}
+            </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-button
+            :disabled="errors.any() || ! user.password"
+            type="submit"
+            variant="primary"
+        >
+        Iniciar sesión
+        
+        </b-button>
+
+    </b-form>    
 </template>
 
 <script>
